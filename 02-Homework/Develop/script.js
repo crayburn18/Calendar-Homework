0332df72013d3d8f7ef57581d7f2a57bd4f5db4c
Connor Rayburn <connorrayburn.work@gmail.com>
2020-04-20 18:13:46 -0700

    set local storage and appended colors to each slot based on time

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,21 +1,64 @@
-let tdyDate = $("#currentDay")
 
-tdyDate.text(moment().format("LLLL"));
 
-let currentTime = moment().hour();
 
-let hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+$(document).ready(function(){
+  
+    let currentTime = moment().hour();
 
-for(i=0; i>hour.length; i++){
-    let calendarTime = $("#" + hour[i]);
 
-    if (currentTime == hour[i]){
-        $("#" + hour[i]).addClass("present");
-    } 
-    else if (currentTime > hour[i]){
-        $("#" + hour[i]).addClass("past");
-    } else {
-        $("#" + hour[i]).addClass("future");
+    $(".saveData").on("click", function(){
+
+        var textValue = $(this).siblings(".inputBox").val();
+        var parentId = $(this).parent().attr("id")
+
+        localStorage.setItem(parentId, textValue);
+
+
+    });
+
+
+    $(".inputBox").each(function(){
+        var parentId = $(this).parent().attr("id")
+        var value = localStorage.getItem(parentId);
+
+        $(this).val(value);
+
+    });
+
+
+   
+    changeInputColor(); 
+    var interval = setInterval(changeInputColor, 1500)
+    function changeInputColor(){
+
+
+        $(".inputBox").each(function(){
+            var parentId = parseInt($(this).parent().attr("id"));
+            if(parentId < currentTime){
+                $(this).removeClass("present")
+                $(this).removeClass("future")
+                $(this).addClass("past")
+            } else if(parentId === currentTime){
+                $(this).addClass("present")
+                $(this).removeClass("future")
+                $(this).removeClass("past")
+            }else{
+                $(this).removeClass("present")
+                $(this).addClass("future")
+                $(this).removeClass("past")
+            }
+    
+        });
+
     }
-};
 
+    getDateTime()
+    setInterval(getDateTime, 100)
+
+    function getDateTime(){
+        let tdyDate = $("#currentDay").text("");
+        tdyDate.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }
+    
+
+});
